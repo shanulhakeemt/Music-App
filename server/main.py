940423a76e593c55900227b5c05e81736f25a809
shanul hakeem <shanulhakeem60@gmail.com>
2024-10-06 13:01:58 +0530

    .

--- server/main.py
+++ server/main.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI
+from models.base import Base
+from pydantic_schemas.user_create import UserCreate
+from routes import auth, song
+from database import engin
+import uvicorn
+import os
+
+app = FastAPI()
+app.include_router(auth.router, prefix='/auth')
+app.include_router(song.router, prefix='/song')
+
+Base.metadata.create_all(engin)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Use PORT from environment, default to 8000 for local testing
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+
+
 # from fastapi import FastAPI
 # from models.base import Base
 # from pydantic_schemas.user_create import UserCreate
@@ -15,22 +37,21 @@
 
 # uvicorn.run(app, host="192.168.225.63", port=8000)
    
-#!/usr/bin/env python3
 
 
-from fastapi import FastAPI
-from models.base import Base
-from pydantic_schemas.user_create import UserCreate
-from routes import auth, song
-from database import engin
-import uvicorn
+# from fastapi import FastAPI
+# from models.base import Base
+# from pydantic_schemas.user_create import UserCreate
+# from routes import auth, song
+# from database import engin
+# import uvicorn
 
 
-app = FastAPI()
-app.include_router(auth.router, prefix='/auth')
-app.include_router(song.router, prefix='/song')
+# app = FastAPI()
+# app.include_router(auth.router, prefix='/auth')
+# app.include_router(song.router, prefix='/song')
 
-Base.metadata.create_all(engin)
+# Base.metadata.create_all(engin)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
